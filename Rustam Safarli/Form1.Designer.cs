2e202b8f5f0dd11092e8477f04f92bdee9793586
irtes <irtes@LAPTOP-P6GILUQF>
2022-11-21 08:26:38 +0200

    Add project files.

---
+++ Rustam Safarli/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Rustam_Safarli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(766, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Press on the FIRST CAR to begin the race. | Press THIS SPACE to stop the race. | Press the label above to Start a new game. ";
+            this.textBox1.Click += new System.EventHandler(this.TextBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(728, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ULTIMATE CAR RACE SIMULATOR 2019 (Remastered golden edition God VERSION)";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(782, 28);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(15, 380);
+            this.textBox3.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Rustam_Safarli.Properties.Resources.Cooler_car1;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 286);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(158, 98);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Rustam_Safarli.Properties.Resources.CAR1;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Rustam Safarli/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rustam_Safarli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rand = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            pictureBox1.Left += rand.Next(5, 15);
+            pictureBox2.Left += rand.Next(5, 15);
+            if (pictureBox1.Left > 640)
+            {
+                timer1.Stop();
+                textBox3.BackColor = Color.Blue;
+                MessageBox.Show("That white Fiat Pinto just wrecked this race");
+
+
+            }
+            else if (pictureBox2.Left > 640)
+            {
+                timer1.Stop();
+                textBox3.BackColor = Color.Red;
+                MessageBox.Show("Cooler Red Fiat Pinto won the race!");
+
+            }
+            else if (pictureBox1.Left == 640 || pictureBox2.Left == 640)
+            {
+                timer1.Stop();
+                textBox3.BackColor = Color.Yellow;
+                MessageBox.Show("Go hang youself! (just kidding it's a draw");
+
+            }
+        }
+
+        private void TextBox1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(21, 69);
+            pictureBox2.Location = new Point(21, 286);
+            textBox3.BackColor = Color.White;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
